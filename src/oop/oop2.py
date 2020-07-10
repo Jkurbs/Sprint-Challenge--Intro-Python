65a8ceb65f0349443a5f74835d100a379e899ba4
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-10 11:38:30 -0400

    Fixed motocycle number of wheel assertion error

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels = 2):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
@@ -24,12 +24,15 @@ class GroundVehicle():
 
 class Motorcycle(GroundVehicle): 
 
+
     def __init__(self, num_wheels=2):
         super().__init__(num_wheels=num_wheels)
         self.num_wheels = num_wheels
 
     def drive(self):
         return "BRAAAP!!"
+
+moto = Motorcycle()
 
 
 vehicles = [
