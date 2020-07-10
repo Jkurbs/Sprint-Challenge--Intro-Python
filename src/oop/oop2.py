3fe1784c3731f3145bbb15b5b1045c0642631d43
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-10 11:23:28 -0400

    Write classes for class hierarchies

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,15 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
 
+    def drive(): 
+        return "vroooom"
+
+ground = GroundVehicle()
 
 # Subclass Motorcycle from GroundVehicle.
 #
